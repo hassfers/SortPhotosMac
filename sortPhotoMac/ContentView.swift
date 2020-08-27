3522b0790a6f55d0075e9945116654a9a917c61f
STH <sth@adorsys.de>
2020-08-27 10:19:32 +0200

    add drop feature and recursive file lookup

--- sortPhotoMac/ContentView.swift
+++ sortPhotoMac/ContentView.swift
@@ -9,15 +9,77 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var files: [URL] = []
+    
     var body: some View {
-        Text("Hello, World!")
-            .frame(maxWidth: .infinity, maxHeight: .infinity)
+        VStack{
+            Rectangle()
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .onDrop(of: ["public.file-url"], isTargeted: .constant(true), perform: { provider -> Bool in
+                    print(provider.count)
+                    provider.forEach { (innerProvider) in
+                        innerProvider.loadItem(forTypeIdentifier: "public.file-url", options: nil) { item, error in
+                            guard
+                                let data = item as? Data,
+                                let url = URL(dataRepresentation: data, relativeTo: nil)
+                                else { return }
+                            //                            self.files.append(url)
+                            self.files.append(contentsOf: self.parseFileURLRecursive(url: url))
+                        }
+                    }
+                    return true
+                })
+                .overlay(Text("Drag your files here"))
+            Text("\(files.count)")
+            HStack{
+                Button("Clear"){
+                    self.files.removeAll()
+                }
+                Button("Start"){}
+            }
+            List(files,id: \.self){ file in
+                Text("\(file)")
+                if NSImage(contentsOfFile: file.path) != nil {
+                    Image(nsImage: NSImage(contentsOfFile: file.path)!)
+                        .resizable()
+                        .scaledToFit()
+                }
+                //                Image(nsImage: NSImage(contentsOfFile: file.path)!)
+                //                    .resizable()
+                //                    .scaledToFit()
+            }
+        }
+    }
+    
+    func parseFileURLRecursive(url: URL) ->  [URL] {
+        guard url.isFileURL else { return [] }
+        var urls: [URL] = []
+        
+        if url.hasDirectoryPath {
+            do {
+                let items = try FileManager.default.contentsOfDirectory(atPath: url.path)
+                for item in items {
+                    let url2 = URL(fileURLWithPath: item, relativeTo: url)
+                    if url2.hasDirectoryPath {
+                        urls.append(contentsOf: parseFileURLRecursive(url: url2))
+                    } else {
+                        urls.append(url2)
+                    }
+                }
+            } catch {
+                // failed to read directory – bad permissions, perhaps?
+                return []
+            }
+        } else {
+            return [url]
+        }
+        return urls
     }
 }
 
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView(files: [])
     }
 }
